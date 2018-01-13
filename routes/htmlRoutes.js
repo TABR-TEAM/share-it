89a5c213b19d526f8fa9b83cb73967f13bec2b0d
bryantwofourtwo <bryantwofourtwo@gmail.com>
2018-01-13 12:36:10 -0700

    cleaned up htmlRoutes & server.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -9,19 +9,19 @@ module.exports = function(app) {
     
 // Each of the below routes just handles the HTML page that the user gets sent to.
     
-// index route loads view.html
+// index route loads home page
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
 });
-
+// profile route loads profile page
 app.get("/profile", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/profile.html"));
 });
-
+// categories route loads categories page
 app.get("/categories", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/categories.html"));    
 });
-
+// create_profile route loads create_profile page
 app.get("/create_profile", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/create_profile.html"));
 });
