8805e51d986a099454ddb1bfa228d3a3d64bd96a
bryantwofourtwo <bryantwofourtwo@gmail.com>
2018-01-16 13:27:15 -0700

    create new profile api routes

--- routes/create_new_profile_routes.js
+++ routes/create_new_profile_routes.js
@@ -1,2 +1,61 @@
 
+// api routes for displaying and saving data to // the db
+
+// require models
 var db = require("../models");
+
+// routes
+module.exports = function(app) {
+    // GET route for getting all of the profiles
+    var query = {};
+    app.get("/api/profiles", function(req, res) {
+        if(req.query.profile_id) {
+            query.ProfileId = req.query.profile_id;
+        }
+        db.Post.findAll({
+            where: query,
+            include: [db.Profile]
+        }).then(function(dbProfile) {
+            res.json(dbProfile);
+        });
+    });
+    // GET route for retrieving a single profile
+    app.get("/api/profiles/:id", function(req, res) {
+        db.Profile.findOne({
+            where: {
+                id: req.params.id
+            },
+            include: [dbProfile]
+        }).then(function(dbProfile) {
+            res.json(dbProfile);
+        });
+    });
+    // POST route for saving a new profile
+    app.post("/api/profiles", function(req, res) {
+        db.Profile.create(req.body).then(function(dbProfile) {
+            res.json(dbProfile);
+        });
+    });
+    // DELETE route for deleting a profile
+    app.delete("/api/profiles/:id", function(req, res) {
+        db.Profile.destroy({
+            where: {
+            id: req.params.id
+            }
+        }).then(function(dbProfile) {
+            res.json(dbProfile);
+        });
+    });
+    // PUT route for updating a profile
+    app.put("/api/profiles", function(req, res) {
+        db.Profile.update(
+            req.body,
+            {
+                where: {
+                    id: req.body.id
+                }
+            }).then(function(dbProfile) {
+                res.json(dbProfile);
+            });
+    });
+};
