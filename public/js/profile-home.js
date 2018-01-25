914e6b1dc67893cd3f26efa78288fba8297c5da1
Teega <ricardo.ortega79@gmail.com>
2018-01-25 16:37:55 -0700

    User Tracking and additonal page rendering

--- public/js/profile-home.js
+++ public/js/profile-home.js
@@ -4,6 +4,25 @@ var categoriesCardsDOM = $("#profile-items-own-cards")
 $.get("/session").then(function (res) {
  user = res
  console.log(user)
+  $("#userName").empty()
+  $("#email").empty()
+  $("#userName").text(user.name)
+  $("#email").text(user.email)
+  $.get("/api/categorie/" + user.id).then(function (response) {
+    categoriesCardsDOM.empty()
+    for (var i = 0; i < response.length; i++) {
+
+      var cardDiv = $("<div class='card category-card col-md-3'> </div>");
+      var imgTag = $("<img class='categories-card-image' src=" + response[i].item_image + ">")
+      var cardBlockDiv = $("<div class='card-block'></div>")
+      var hFourTag = $("<h4 class='card-title'> " + response[i].item_name + "</h4>")
+      var pTag = $("<p class='card-text' id='categories-card-text'>Item Description: " + response[i].item_description + "  </p>")
+      cardBlockDiv.append(hFourTag).append(pTag);
+      cardDiv.append(imgTag).append(cardBlockDiv);
+
+      categoriesCardsDOM.append(cardDiv);
+    }
+  })
 })
 console.log(user)
 
--- server.js
+++ server.js
@@ -54,10 +54,11 @@ app.set('view engine', 'handlebars');
 // require("./routes/post-api-routes.js")(app);
 require("./routes/categories_api_routes.js")(app);
 require("./routes/session.js")(app);
+require("./routes/login_route.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync({ force: false }).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
